b453f896e2bb8de710ac0a3d1a5aa8f9437f488f
NovaPUNCH <52199380+NovaPUNCH@users.noreply.github.com>
2019-06-25 13:27:45 +0300

    Add files via upload

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,5 @@
+module.exports = member => {
+    let username = member.user.username;
+    member.sendMessage('https://media.giphy.com/media/ASd0Ukj0y3qMM/giphy.gif Selamlar,Değerli **' + username + '** Discord Sunucuma Hoşgeldin :thinking: Keyifli Vakit Geçirmen Dileğiyle https://gph.is/2VhWm6k :wink:');
+
+};
